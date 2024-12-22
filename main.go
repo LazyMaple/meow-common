95e5b4748faa0921bd933ab58773108168d18075
map1e <root@lazymaple.pw>
2024-12-22 14:40:48 +0000

    update

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"os"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var (
+	Logger *zap.Logger
+)
+
+func init() {
+	Logger = LoadLog()
+}
+
+func LoadLog() *zap.Logger {
+	core := zapcore.NewCore(
+		zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig()),
+		zapcore.AddSync(os.Stdout),
+		zap.DebugLevel,
+	)
+	Logger = zap.New(core)
+	defer Logger.Sync()
+	return Logger
+}
