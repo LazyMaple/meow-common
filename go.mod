81f0c58ce232200c87886118fbec013e65c4be27
map1e <root@lazymaple.pw>
2024-12-22 16:03:07 +0000

    update

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/LazyMaple/meow-common
 go 1.23.4
 
 require (
+	github.com/justincormack/go-memfd v0.0.0-20170219213707-6e4af0518993 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
 	go.uber.org/zap v1.27.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package meow_common
 
 import (
+	"embed"
+	"fmt"
+	"io"
 	"os"
+	"os/exec"
 
+	"github.com/justincormack/go-memfd"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
@@ -32,4 +37,27 @@ func WarpLogError(err error) zap.Field {
 
 func LogError(msg string, err error) {
 	Logger.Error(msg, WarpLogError(err))
+}
+
+func GetCommand(fs embed.FS, filename string, args ...string) (*exec.Cmd, func() error, error) {
+	f, err := fs.Open(filename)
+	if err != nil {
+		return &exec.Cmd{}, nil, err
+	}
+	defer f.Close()
+
+	mfd, err := memfd.Create()
+	if err != nil {
+		return &exec.Cmd{}, nil, err
+	}
+	defer mfd.Unmap()
+
+	_, err = io.Copy(mfd, f)
+	if err != nil {
+		mfd.Close()
+		return &exec.Cmd{}, nil, err
+	}
+
+	cmd := exec.Command(fmt.Sprintf("/proc/self/fd/%d", mfd.Fd()), args...)
+	return cmd, mfd.Close, nil
 }
